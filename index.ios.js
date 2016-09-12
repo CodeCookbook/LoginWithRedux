340f0533e0922701a2d817225a2739fa0dd3edcb
kmdupr33 <kmdupr33@gmail.com>
2016-09-12 09:57:07 -0400

    Revert "Create and wire up container component for username field"

--- index.ios.js
+++ index.ios.js
@@ -28,52 +28,18 @@ import {
   TextInput
 } from 'react-native';
 
-import { createStore } from 'redux';
-import { connect, Provider } from 'react-redux';
-
-const TYPE_USERNAME = 'TYPE_USERNAME';
-const typeUsername = (text) => ({
-  type: TYPE_USERNAME,
-  text
-})
-
-const LoginFormTextInput = connect((state) => ({
-  value: state.username
-}), (dispatch) => ({
-  onChangeText: (text) => {
-    dispatch(typeUsername(text));
-  }
-}))(TextInput)
-
 class LoginWithRedux extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      pendingLoginRequest: false
-    };
-    this.store = createStore((state, action) => {
-      console.log(JSON.stringify(action));
-      return {...state, username: action.text}
-    }, this.state);
-  }
-
-
   render() {
     return (
-      <Provider store={this.store}>
-        <View style={styles.container}>
-        <LoginFormTextInput style={styles.textInput} placeholder="Username"/>
-        <TextInput style={styles.textInput} placeholder="Password"/>
-        <TouchableHighlight style={{padding: 15, backgroundColor: '#fed136'}}>
-          <Text style={styles.welcome}>
-            Get Cookin'
-          </Text>
-        </TouchableHighlight>
-        </View>
-      </Provider>
+      <View style={styles.container}>
+      <TextInput style={styles.textInput} placeholder="Username"/>
+      <TextInput style={styles.textInput} placeholder="Password"/>
+      <TouchableHighlight style={{padding: 15, backgroundColor: '#fed136'}}>
+        <Text style={styles.welcome}>
+          Get Cookin'
+        </Text>
+      </TouchableHighlight>
+      </View>
     );
   }
 }
